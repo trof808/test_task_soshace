6d8fd9ba708bfe992d5f37211969c62b193899be
trof <reatrof@gmail.com>
2017-06-30 17:38:45 +0300

    webpack config, client side

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+class Root extends Component {
+    render() {
+        return (
+            <div>Hi</div>
+        )
+    }
+}
+
+export default Root;
